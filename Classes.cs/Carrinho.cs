a2b0442aeb4ef04aaa3e16828b3c955436c9ceff
guhenriofc <you@example.com>
2020-12-17 17:09:59 -0200

    Classe carrinho em execução

--- Classes.cs/Carrinho.cs
+++ Classes.cs/Carrinho.cs
@@ -1,7 +1,30 @@
+using System;
+using System.Collections.Generic;
+
 namespace Objeto_Argumento.Classes.cs
 {
     public class Carrinho
     {
+        public float ValorTotal { get; set; }
+
+        List<Produto> _carrinho = new List<Produto>();
+
+        public void AdicionarProduto(Produto _produto){
+            _carrinho.Add(_produto);
+        }
+
+        public void MostrarProduto(){
+            if(_carrinho != null){
+                foreach (Produto item in _carrinho)
+                {
+                    Console.WriteLine($"Item -> {item.Nome} Codigo -> {item.Codigo} Preco -> {item.Preco}");
+                }
+            }
+        }
+
+        public void RemoverProduto(Produto _produto){
+            _carrinho.Remove(_produto);
+        }
         
     }
 }
--- Classes.cs/Produto.cs
+++ Classes.cs/Produto.cs
@@ -2,6 +2,19 @@ namespace Objeto_Argumento.Classes.cs
 {
     public class Produto
     {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+        public Produto(){
+            
+        }
+        public Produto(int _codigo, string _nome, float _preco){
+            this.Codigo = _codigo;
+            this.Nome = _nome;
+            this.Preco = _preco;
+        }
+        
         
     }
 }
